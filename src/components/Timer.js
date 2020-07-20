e6ab8b0820eb12c84c8489efa8b28b5ff3b4b5e0
Mxkert <mikehendriks94@gmail.com>
2020-07-20 20:28:33 +0200

    Start app

---
+++ src/components/Timer.js
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from 'react';
+import './Timer.css';
+
+const Timer = () => {
+  const [seconds, setSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+  const [pauses, setPauses] = useState([
+    10, 20, 30
+  ])
+
+  function toggle() {
+    setIsActive(!isActive);
+  }
+
+  function reset() {
+    setSeconds(0);
+    setIsActive(false);
+  }
+ 
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setSeconds(seconds => seconds + 1);
+      }, 1000);
+      
+      pauses.map(pause => {
+        if (seconds === pause) {
+          if ("Notification" in window && Notification.permission === "granted") {
+            new Notification(pause + 's verstreken');
+          }
+        }
+      })
+
+    } else if (!isActive && seconds !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, seconds]);
+
+  function requestNotificationPermission() {
+    // Some browsers don't support Notification yet. I'm looking at you iOS Safari
+    if ("Notification" in window) {
+      if (
+        Notification.permission !== "denied" &&
+        Notification.permission !== "granted"
+      ) {
+        Notification.requestPermission();
+      }
+    }
+  }
+
+  return (
+    <div className="app">
+      <div className="time">
+        {seconds}s
+      </div>
+      <div className="row">
+        <button className={`button button-primary button-primary-${isActive ? 'active' : 'inactive'}`} onClick={toggle}>
+          {isActive ? 'Pause' : 'Start'}
+        </button>
+        <button className="button" onClick={reset}>
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Timer;
